8facf2eeaddbc8583ac25a7b6369c27691170045
Nick Maludy <nmaludy@gmail.com>
2020-06-24 08:51:34 -0400

    Cleaned up unused imports. Fixed some formatting things. Converted some loops to list comprehensions.

--- actions/with_items.py
+++ actions/with_items.py
@@ -1,35 +1,28 @@
-import traceback
 import copy
-import argparse
-import logging
-import json
 import jinja2
+import re
 import six
-import types
-import pdb
-from utils.verify import set_verify
-from utils.exceptions import RateLimitException, WithException
-from virus_total_apis import PublicApi, ApiError
-# all async sockets
+import time
+import urllib3
+
 from st2client.models import LiveAction
 from st2client.client import Client
 from st2client.commands import action as st2action
-import urllib3
-import re
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-import time
 from st2common.runners.base_action import Action
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 JINJA_REGEXP = '({{(.*?)}})'
 
-class Process(Action):
+class WithItemsAction(Action):
+
     def __init__(self, config):
         super(Process, self).__init__(config)
         self.jinja_env = jinja2.Environment()
         self.jinja_pattern = re.compile(JINJA_REGEXP)
 
     def unescape_jinja(self, expr):
-        if isinstance(expr, str):
+        if isinstance(expr, six.string_types):
             return self.unescape_jinja_str(expr)
         elif isinstance(expr, list):
             return self.unescape_jinja_list(expr)
@@ -44,11 +37,8 @@ class Process(Action):
         return expr_str
 
     def unescape_jinja_list(self, expr_list):
-        result_list = []
-        for expr in expr_list:
-            expr = self.unescape_jinja(expr)
-            result_list.append(expr)
-        return result_list
+        return [self.unescape_jinja(expr) for expr in expr_list ]
+
     def unescape_jinja_dict(self, expr_dict):
         for k, v in six.iteritems(expr_dict):
             expr_dict[k] = self.unescape_jinja(v)
@@ -83,48 +73,36 @@ class Process(Action):
             return template.render(context)
 
     def render_jinja_list(self, context, expr_list):
-        rendered = []
-        for expr in expr_list:
-            expr = self.render_jinja(context, expr)
-            rendered.append(expr)
-        return rendered
+        return [self.render_jinja(context, expr) for expr in expr_list]
 
     def render_jinja_dict(self, context, expr_dict):
         rendered = {}
         for k, expr in six.iteritems(expr_dict):
             rendered[k] = self.render_jinja(context, expr)
         return rendered
-    def run(self, *args, **kwargs):
-        action = kwargs["action"]
-        list_params = kwargs["parameters"]
-        logger = self.logger
-        config = self.config
-        paging_limit = kwargs["paging_limit"]
-        sleep_time = kwargs["sleep_time"]
-        result_expr = kwargs.get("result",None)
 
+    def run(self, action, parameters, paging_limit, sleep_time, result=None, **kwargs):
         # unescape jinja
-
+        result_expr = result
         if result_expr:
             result_expr = self.unescape_jinja(result_expr)
+
         # rate limit
-        client = Client(base_url=config["st2baseurl"],
-                auth_url=config["st2authurl"],
-                #debug=True,
-                api_url=config["st2apiurl"]
-                )
+        client = Client(base_url=self.config["st2baseurl"],
+                        auth_url=self.config["st2authurl"],
+                        api_url=self.config["st2apiurl"])
         running_ids = []
         finished = []
         param_index = 0
         success = True
-        while len(finished) < len(list_params):
+        while len(finished) < len(parameters):
             # fill paging pool
             while len(running_ids) < paging_limit and \
-                    len(list_params) != len(running_ids) + len(finished) : # end of list no more to page
-                logger.debug("creating action index: " + str(param_index))
+                    len(parameters) != len(running_ids) + len(finished) : # end of list no more to page
+                self.logger.debug("creating action index: " + str(param_index))
                 execution = client.executions.create(
                     LiveAction(action=action,
-                        parameters=list_params[param_index]))
+                               parameters=parameters[param_index]))
                 running_ids.append(execution.id)
                 param_index+=1
 
@@ -136,10 +114,11 @@ class Process(Action):
             for e_updated in execution_list:
                 if e_updated.status in st2action.LIVEACTION_COMPLETED_STATES:
                     if e_updated.status != st2action.LIVEACTION_STATUS_SUCCEEDED:
-                        success=False
-                    logger.debug("finished execution: " + str(e_updated.id))
+                        success = False
+                    self.logger.debug("finished execution: " + str(e_updated.id))
                     running_ids.remove(e_updated.id)
                     finished.append(e_updated)
+
         # extract the information out of the output
         outputs = []
         for exe in finished:
